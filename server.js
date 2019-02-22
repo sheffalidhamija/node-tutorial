58bab3fb753e120225a64d3f090cb1d2a37afac8
i322274 <s.dhamija@sap.com>
2019-02-22 17:30:56 +0530

    Setup Start Script & Heroku Port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs'); 
 const fs = require('fs');
+const port = process.env.PORT || 3000;
+
 
 var app = express();
 
@@ -59,6 +61,6 @@ app.get('/bad', (request,response) =>{
      errorMessage : 'Unable To Connect'
  });
 });
-app.listen(3000, () =>{
-    console.log('Server is running on port 3000');
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
 });
